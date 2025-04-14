f5becc55ec7b52ec48a34177956d50496aad1766
aman <asaxena7531@gmail.com>
2025-04-14 13:43:11 +0530

    config env in config

--- app/config/db.js
+++ app/config/db.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+console.log("Connecting to database... with env", process.env.DB_URL);
 const connectDB = async () => {
   try {
     const result = await mongoose.connect(process.env.DB_URL);
