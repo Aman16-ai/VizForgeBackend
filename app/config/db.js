17bdae57b1aceb2b33c419252685de06943337a8
aman <asaxena7531@gmail.com>
2025-04-14 13:34:25 +0530

    config env in config

--- app/config/db.js
+++ app/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
-    const result = await mongoose.connect(process.env.db_url);
+    const result = await mongoose.connect(process.env.DB_URL);
     console.log("Database connected successfully");
   } catch (err) {
     console.log(err);
